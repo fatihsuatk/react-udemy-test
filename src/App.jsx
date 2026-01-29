e94d5fb274d176242649ca14d6f34b3c0d7b4c3e
Fatih Suat <fatihskabakci@gmail.com>
2026-01-30 00:36:22 +0300

    abaragumba

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <><h1>Learn React with Chaicode.com</h1><p>Napan gardaşlar!</p></>
+    <><h1>Learn React with Chaicode.com</h1><p>Napan gardaşlarrrr!</p></>
   )
 }
 
