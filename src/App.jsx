8a873694435edf8ad831a3f397a12fbfd12adc6a
Fatih Suat <fatihskabakci@gmail.com>
2026-01-30 00:27:02 +0300

    change subheading

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <><h1>Learn React with Chaicode.com</h1><p>This is a sub heading</p></>
+    <><h1>Learn React with Chaicode.com</h1><p>Napan gardaş!</p></>
   )
 }
 
